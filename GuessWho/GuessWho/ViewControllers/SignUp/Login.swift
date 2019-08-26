1a86f619531348d327b36ab95590faee9a82e9be
Andrew Ruiz <nukedoom@gmail.com>
2019-08-26 14:19:34 -0700

    Added view controllers and view controller files for sign up and login screens

---
+++ GuessWho/GuessWho/ViewControllers/SignUp/Login.swift
@@ -0,0 +1,9 @@
+//
+//  Login.swift
+//  GuessWho
+//
+//  Created by Andrew Ruiz on 8/26/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+import Foundation
---
+++ GuessWho/GuessWho/ViewControllers/SignUp/MainMenu.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpLoginMenu.swift
+//  GuessWho
+//
+//  Created by Andrew Ruiz on 8/26/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+import Foundation
