b8eec08e251942b3700e623387b2dc0543ed7192
Andrew Ruiz <nukedoom@gmail.com>
2019-08-27 13:33:22 -0700

    Initial Commit

---
+++ GuessWho/ModelController/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  GuessWho
+//
+//  Created by Luqmaan Khan on 8/26/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+import Foundation
---
+++ GuessWho/ViewControllers/SignUp/MainMenu.swift
@@ -0,0 +1,27 @@
+//
+//  SignUpLoginMenu.swift
+//  GuessWho
+//
+//  Created by Andrew Ruiz on 8/26/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: UIViewController {
+    
+    @IBOutlet weak var gameIcon: UIImageView!
+    @IBOutlet weak var gameTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+}
